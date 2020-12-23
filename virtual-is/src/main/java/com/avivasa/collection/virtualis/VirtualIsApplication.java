8d15928523e4ce3c8e68f39a2cfc7fde0e7c034f
ozsoy <leventozsoy@gmail.com>
2020-12-23 16:15:49 +0300

    Add files via upload

---
+++ virtual-is/src/main/java/com/avivasa/collection/virtualis/VirtualIsApplication.java
@@ -0,0 +1,13 @@
+package com.avivasa.collection.virtualis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualIsApplication.class, args);
+	}
+
+}
---
+++ virtual-is/src/test/java/com/avivasa/collection/virtualis/VirtualIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avivasa.collection.virtualis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
